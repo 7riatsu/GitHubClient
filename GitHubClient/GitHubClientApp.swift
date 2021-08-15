a60c861b23cc97e9a159fc9cfb6bb470c4c913b5
7riatsu <atsuki02jojo@gmail.com>
2021-08-15 16:44:29 +0900

    Initial Commit

---
+++ GitHubClient/GitHubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientApp.swift
+//  GitHubClient
+//
+//  Created by 成田篤基 on 2021/08/15.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
